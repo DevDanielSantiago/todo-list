5caff7ec06fb6ab8838c72355567d5bff6161a6a
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-26 12:23:07 -0400

    chore: create list mock

---
+++ src/core/mocks/lists.ts
@@ -0,0 +1,36 @@
+import { List } from "../interfaces/list";
+
+export const Lists: List[] = [
+  {
+    id: "1",
+    title: "Minha Lista 1",
+    activities: [],
+  },
+  {
+    id: "2",
+    title: "Minha Lista 2",
+    activities: [
+      {
+        id: "1",
+        name: "Nome da Atividade",
+        description: null,
+        concludedAt: null,
+        deadline: null,
+      },
+      {
+        id: "2",
+        name: "Nome da Atividade 2",
+        description: "Descrição da atividade 2",
+        concludedAt: new Date(),
+        deadline: null,
+      },
+      {
+        id: "3",
+        name: "Nome da Atividade 3",
+        description: "Descrição da atividade 3",
+        concludedAt: null,
+        deadline: new Date(),
+      },
+    ],
+  },
+];
