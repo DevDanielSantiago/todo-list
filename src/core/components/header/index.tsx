78a94290a261eb356c2f41bdb0853bcfd3ffb5b1
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-26 16:10:07 -0400

    feat: add prop onAdd to handle with some creation

--- src/core/components/header/index.tsx
+++ src/core/components/header/index.tsx
@@ -5,13 +5,16 @@ import "./styles.css";
 interface HeaderComponentProps {
   title: string;
   buttonText: string;
+  onAdd?: () => void;
 }
 
-function HeaderComponent({ title, buttonText }: HeaderComponentProps) {
+function HeaderComponent({ title, buttonText, onAdd }: HeaderComponentProps) {
   return (
     <div className="c-header">
       <h1 className="c-header_title">{title}</h1>
-      <button className="c-header_button">{buttonText}</button>
+      <button className="c-header_button" onClick={onAdd}>
+        {buttonText}
+      </button>
     </div>
   );
 }
