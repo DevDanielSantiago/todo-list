7c68f86088bee216b75030b4d7178e082a76f83e
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-26 13:47:54 -0400

    feat: add list page and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./modules/pages/home";
+import ListPage from "./modules/pages/list";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/list/:id" element={<ListPage />} />
       </Routes>
     </Router>
   );
---
+++ src/modules/pages/list/index.tsx
@@ -0,0 +1,7 @@
+import ListTemplate from "../../templates/list";
+
+function ListPage() {
+  return <ListTemplate />;
+}
+
+export default ListPage;
