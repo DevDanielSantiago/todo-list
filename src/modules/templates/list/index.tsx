92aa652f69cc767cdaa7733d64d3a13198b702a1
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-26 13:47:31 -0400

    feat: add list template

---
+++ src/modules/templates/list/index.tsx
@@ -0,0 +1,26 @@
+import { Activities } from "../../../core/mocks/activities";
+
+import ActivityComponent from "./activity";
+
+import ContainerComponent from "../../../core/components/container";
+import HeaderComponent from "../../../core/components/header";
+
+function ListTemplate() {
+  return (
+    <ContainerComponent>
+      <HeaderComponent title="Lista de Atividades" buttonText="Adicionar" />
+
+      {Activities.map((activity, index) => (
+        <ActivityComponent
+          key={index}
+          name={activity.name}
+          description={activity.description}
+          deadline={activity.deadline}
+          concludedAt={activity.concludedAt}
+        />
+      ))}
+    </ContainerComponent>
+  );
+}
+
+export default ListTemplate;
