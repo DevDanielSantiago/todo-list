aabb81234bf2b15d1ebd0bd2a6f9a60cccc7cc05
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-27 08:04:20 -0400

    feat: create method to find todo lists

--- src/modules/pages/home/index.tsx
+++ src/modules/pages/home/index.tsx
@@ -1,6 +1,41 @@
+import { useEffect } from "react";
 import HomeTemplate from "../../templates/home";
 
+import { List } from "../../../core/interfaces/list";
+
+import { useDbService } from "../../../core/services/db.service";
+import { useBetStore } from "../../../core/stores/list";
+
 function HomePage() {
+  const { setTodoLists } = useBetStore();
+  const { findAll } = useDbService();
+
+  const handleList = async () => {
+    try {
+      const response = await findAll();
+      const lists = await response.json();
+
+      const todoLists: List[] = lists.lists.map((list: any) => {
+        return {
+          id: list.id,
+          title: list.title,
+          activities: lists.activities.filter(
+            (act: any) => act.listId === list.id
+          ),
+        };
+      });
+
+      setTodoLists(todoLists);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <HomeTemplate />;
 }
 
