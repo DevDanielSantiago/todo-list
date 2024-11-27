4fa2d3be97a2a6260d1edd3150d1d026dc9530b0
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-27 08:06:06 -0400

    feat: implement list search based on the ID parameter coming from the list

--- src/modules/pages/list/index.tsx
+++ src/modules/pages/list/index.tsx
@@ -1,7 +1,65 @@
-import ListTemplate from "../../templates/list";
+import { atom, useAtom } from "jotai";
+import { lazy, Suspense, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { useDbService } from "../../../core/services/db.service";
+import { List } from "../../../core/interfaces/list";
+import { useBetStore } from "../../../core/stores/list";
+
+const LazyListTemplate = lazy(() => import("../../templates/list"));
+
+const loadingAtom = atom(false);
 
 function ListPage() {
-  return <ListTemplate />;
+  const [loading, setLoading] = useAtom(loadingAtom);
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { setList } = useBetStore();
+  const { findListById, findActivitiesByListId } = useDbService();
+
+  const handleList = async (id: string) => {
+    try {
+      setLoading(true);
+      const response = await Promise.all([
+        findListById(id),
+        findActivitiesByListId(id),
+      ]);
+      const lists = await Promise.all(response.map((r) => r.json()));
+
+      if (lists[0].length === 0) return navigate("/");
+      const todoList: List = {
+        ...lists[0][0],
+        id: lists[0][0].id,
+        title: lists[0][0].title,
+        activities: lists[1].map((act: any) => ({
+          ...act,
+          id: act.id,
+          name: act.name,
+          description: act.description,
+          concludedAt: act.concludedAt,
+        })),
+      };
+
+      setList(todoList);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (id) handleList(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  return (
+    <Suspense fallback={<p>Carregando página...</p>}>
+      <LazyListTemplate loading={loading} />
+    </Suspense>
+  );
 }
 
 export default ListPage;
